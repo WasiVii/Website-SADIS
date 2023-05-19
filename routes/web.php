40cc2c8825566c817b61fa42f35efd7be79ef348
Abdul Wasi <abdul.wasi.al.afif@gmail.com>
2023-05-19 20:02:15 +0700

    Add blade and routing home

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('layouts.app');
+    return view('layouts.home');
 });
 
 Route::prefix('profile')->group(function () {
