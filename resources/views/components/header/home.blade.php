56b6bc1e575e159480bc292a20626c889d230548
Abdul Wasi <abdul.wasi.al.afif@gmail.com>
2023-05-19 19:12:39 +0700

    Add Menu Navbar

--- resources/views/components/header/home.blade.php
+++ resources/views/components/header/home.blade.php
@@ -1,6 +1,95 @@
-<header>
-    <div class="max-w-full">
-        <p>Header</p>
-        <p>Header</p>
+<header class="shadow">
+    <div class="navbar bg-base-100">
+        <div class="navbar-start">
+            <div class="dropdown">
+            <label tabindex="0" class="btn btn-ghost lg:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </label>
+            <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                <li><a href="/">Home</a></li>
+                <li tabindex="0">
+                    <a class="justify-between">
+                        Profil
+                        <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
+                    </a>
+                    <ul class="p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><a href="/profile/sekolah">Sekolah</a></li>
+                        <li><a href="/profile/visi-misi">Visi dan Misi</a></li>
+                        <li><a href="/profile/data-guru">Data Guru</a></li>
+                        <li><a href="/profile/peta">Peta Sekolah</a></li>
+                    </ul>
+                </li>
+                <li tabindex="0">
+                    <a class="justify-between">
+                        Informasi
+                        <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
+                    </a>
+                    <ul class="p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><a href="/news">Berita Kegiatan</a></li>
+                        <li><a href="/kurikulum">Kurikulum</a></li>
+                        <li><a href="/extrakulikuler">Extrakulikuler</a></li>
+                        <li><a href="/sarana-prasarana">Sarana & Prasarana</a></li>
+                    </ul>
+                </li>
+                <li tabindex="0">
+                    <a class="justify-between">
+                        Kesiswaan
+                        <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
+                    </a>
+                    <ul class="p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><a href="/siswa">Data Siswa</a></li>
+                        <li><a href="/ppdb">PPDB</a></li>
+                        <li><a href="/login">Login</a></li>
+                    </ul>
+                </li>
+                <li><a href="/galeri">Galeri</a></li>
+            </ul>
+            </div>
+            <a class="btn btn-ghost normal-case text-xl" href="/">SADISschool</a>
+        </div>
+        <div class="navbar-center hidden lg:flex">
+            <ul class="menu menu-horizontal px-1">
+            <li><a href="/">Home</a></li>
+            <li tabindex="0">
+                <a>
+                    Profil
+                <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+                </a>
+                <ul class="p-2 shadow bg-base-100 rounded-box w-52">
+                    <li><a href="/profile/sekolah">Sekolah</a></li>
+                    <li><a href="/profile/visi-misi">Visi dan Misi</a></li>
+                    <li><a href="/profile/data-guru">Data Guru</a></li>
+                    <li><a href="/profile/peta">Peta Sekolah</a></li>
+                </ul>
+            </li>
+            <li tabindex="0">
+                <a>
+                    Informasi
+                <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+                </a>
+                <ul class="p-2 shadow bg-base-100 rounded-box w-52">
+                    <li><a href="/news">Berita Kegiatan</a></li>
+                    <li><a href="/kurikulum">Kurikulum</a></li>
+                    <li><a href="/extrakulikuler">Extrakulikuler</a></li>
+                    <li><a href="/sarana-prasarana">Sarana & Prasarana</a></li>
+                </ul>
+            </li>
+            <li tabindex="0">
+                <a>
+                    Kesiswaan
+                <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+                </a>
+                <ul class="p-2 shadow bg-base-100 rounded-box w-52">
+                    <li><a href="/siswa">Data Siswa</a></li>
+                    <li><a href="/ppdb">PPDB</a></li>
+                    <li><a href="/login">Login</a></li>
+                </ul>
+            </li>
+            <li><a href="/galeri">Galeri</a></li>
+            </ul>
+        </div>
+        <div class="navbar-end">
+            {{-- <a class="btn">Get started</a> --}}
+        </div>
     </div>
 </header>
