73c8aaebb016c2b786f1ee5337c95c9a13cd92bf
Abdul Wasi <abdul.wasi.al.afif@gmail.com>
2023-05-23 20:13:56 +0700

    Add Galeri Interface

--- resources/views/layouts/galeri.blade.php
+++ resources/views/layouts/galeri.blade.php
@@ -2,14 +2,56 @@
  
 @section('title', 'Galeri')
 
+@php
+$galeri1 = array(
+    'image' => '/assets/img/galeri/galeri1.png'
+);
+$galeri2 = array(
+    'image' => '/assets/img/galeri/galeri2.png'
+);
+$galeri3 = array(
+    'image' => '/assets/img/galeri/galeri3.png'
+);
+$galeri4 = array(
+    'image' => '/assets/img/galeri/galeri4.png'
+);
+$galeri5 = array(
+    'image' => '/assets/img/galeri/galeri5.png'
+);
+$data_images = [$galeri1, $galeri2, $galeri3, $galeri4, $galeri5];
+@endphp
+
 @section('content')
-    <section class="py-10 sm:pt-16 lg:pt-24">
+    <section class="flex flex-col min-h-screen py-10">
         <div class="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
-            <div class="grid grid-cols-2 md:col-span-3 lg:grid-cols-6 gap-y-16 gap-x-12">
-                {{-- INI AWAL CONTENT --}}
-                <p>isi Galeri disini</p>
-                {{-- INI AKHIR CONTENT --}}
-            </div>
+            <section class="py-10 bg-main-color rounded-lg max-w-7xl">
+                <div class="px-4 mx-auto sm:px-6 lg:px-8">
+                    <div class="max-w-2xl mx-auto text-center">
+                    <p class="mt-5 text-4xl font-bold leading-tight text-gray-900 sm:leading-tight sm:text-5xl lg:text-6xl lg:leading-tight font-pj">
+                        Galeri Foto Sekolah
+                    </p>
+                    </div>
+                </div>
+            </section>
+            <section class="mt-5 card card-side bg-base-100 shadow-xl container mx-auto rounded-lg max-w-7xl">
+                <section class="flex flex-col mt-5 card card-side bg-base-100 shadow-xl container mx-auto rounded-lg">
+                <section class="container mx-auto">
+                    <div class="m-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 md:gap-4 lg:gap-6">
+                        @foreach ($data_images as $galeri)
+                        <div class="card mt-5 bg-main-color shadow-xl">
+                            <figure class="px-10 pt-10">
+                                <img
+                                    class="h-60"
+                                    src="{{ $galeri['image'] }}"
+                                    alt=""
+                                />
+                            </figure>
+                        </div>
+                        @endforeach
+                    </div>
+                </section>
+                </section>
+            </section>
         </div>
     </section>
 @endsection
