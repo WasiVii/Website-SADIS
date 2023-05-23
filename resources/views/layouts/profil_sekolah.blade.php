1d8462df0128e01a5471c1ea643a10667afc5bc8
Abdul Wasi <abdul.wasi.al.afif@gmail.com>
2023-05-23 08:02:04 +0700

    Update Blade Main Content Layout

--- resources/views/layouts/profil_sekolah.blade.php
+++ resources/views/layouts/profil_sekolah.blade.php
@@ -3,6 +3,14 @@
 @section('title', 'Profil Sekolah')
 
 @section('content')
-    <p>ini Profil Sekolah</p>
+    <section class="py-10 sm:pt-16 lg:pt-24">
+        <div class="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+            <div class="grid grid-cols-2 md:col-span-3 lg:grid-cols-6 gap-y-16 gap-x-12">
+                {{-- INI AWAL CONTENT --}}
+                <p>isi Profil Sekolah disini</p>
+                {{-- INI AKHIR CONTENT --}}
+            </div>
+        </div>
+    </section>
 @endsection
 
