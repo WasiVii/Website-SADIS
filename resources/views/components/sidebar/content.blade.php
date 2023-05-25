c07ccb3c8b96d72faeb760021856ddb58af140c2
Abdul Wasi <abdul.wasi.al.afif@gmail.com>
2023-05-25 11:06:32 +0700

    Fix Admin Menu

--- resources/views/components/sidebar/content.blade.php
+++ resources/views/components/sidebar/content.blade.php
@@ -4,6 +4,15 @@
     class="flex flex-col flex-1 gap-4 px-3"
 >
 
+    <a
+        href="{{ route('dashboard') }}"
+        class="inline-flex items-center gap-2"
+    >
+        <x-application-logo aria-hidden="true" class="h-22 sm:h-auto lg:h-auto" />
+
+        <span class="sr-only">Dashboard</span>
+    </a>
+
     <x-sidebar.link
         title="Dashboard"
         href="{{ route('dashboard') }}"
@@ -15,7 +24,7 @@
     </x-sidebar.link>
 
     <x-sidebar.dropdown
-        title="Master Data"
+        title="Administrasi Data"
         :active="Str::startsWith(request()->route()->uri(), 'buttons')"
     >
         <x-slot name="icon">
@@ -43,20 +52,42 @@
             :active="request()->routeIs('mapel.index')"
         />
         <x-sidebar.sublink
+            title="Data Nilai"
+            href="{{ route('nilai.index') }}"
+            :active="request()->routeIs('nilai.index')"
+        />
+        <x-sidebar.sublink
             title="Data Staff"
             href="{{ route('staff.index') }}"
             :active="request()->routeIs('staff.index')"
         />
     </x-sidebar.dropdown>
-    <x-sidebar.sublink
+    <x-sidebar.dropdown
+        title="Website Data"
+        :active="Str::startsWith(request()->route()->uri(), 'buttons')"
+    >
+        <x-slot name="icon">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-3d-cube-sphere" width="22" height="22" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M6 17.6l-2 -1.1v-2.5" />
+                <path d="M4 10v-2.5l2 -1.1" />
+                <path d="M10 4.1l2 -1.1l2 1.1" />
+                <path d="M18 6.4l2 1.1v2.5" />
+                <path d="M20 14v2.5l-2 1.12" />
+                <path d="M14 19.9l-2 1.1l-2 -1.1" />
+                <path d="M12 12l2 -1.1" />
+                <path d="M18 8.6l2 -1.1" />
+                <path d="M12 12l0 2.5" />
+                <path d="M12 18.5l0 2.5" />
+                <path d="M12 12l-2 -1.12" />
+                <path d="M6 8.6l-2 -1.1" />
+            </svg>
+        </x-slot>
+
+        <x-sidebar.sublink
             title="Data Sarana"
             href="{{ route('sarana.index') }}"
             :active="request()->routeIs('sarana.index')"
-        />
-        <x-sidebar.sublink
-            title="Data Nilai"
-            href="{{ route('nilai.index') }}"
-            :active="request()->routeIs('nilai.index')"
         />
         <x-sidebar.sublink
             title="Data Ekstrakulikuler"
@@ -68,4 +99,5 @@
             href="{{ route('berita.index') }}"
             :active="request()->routeIs('berita.index')"
         />
+    </x-sidebar.dropdown>
 </x-perfect-scrollbar>
