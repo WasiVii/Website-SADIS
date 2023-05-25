a92360d096f30c471071da12f91887bc4bb34992
Helfi23 <fie230401@gmail.com>
2023-05-25 10:34:34 +0700

    Update Berita dan ekstrakulikuler  Sudah Done

--- resources/views/Ekstrakulikuler/show.blade.php
+++ resources/views/Ekstrakulikuler/show.blade.php
@@ -1,24 +1,38 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight text-gray-800">
-            {{ __('Ekstrakulikuler Show') }}
+            {{ __('Show Ekstrakulikuler') }}
         </h2>
     </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
-            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-
-                    <div class="flex flex-col items-center justify-center">
-                        <h1 class="text-2xl font-bold">{{ $ekstrakulikulers->Nama_Ekstrakulikuler }}</h1>
-                        <div>{!! $ekstrakulikulers->Deskripsi !!}</div>
-                        <div>{!! $ekstrakulikulers->Photo !!}</div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+        <div class="card">
+        <div class="card-header">{{ $ekstrakulikuler->Judul }}</div>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-3">
+                    @empty($ekstrakulikuler->photo)
+                                        <img src="{{ url('images/nophoto.jpg') }}" alt="">
+                                        @else
+                                        <img src="{{ url('images') }}/{{ $ekstrakulikuler->photo }}" alt="" class="w-12 h-12">
+                                        @endempty
+                </div>
+                <div class="col-md-9">
+                    <div class="form-group">
+                        <label for="Nama_Ekstrakulikuler">Nama Ekstrakulikuler</label>
+                        <input type="text" class="form-control" id="Nama_Ekstrakulikuler" name="Nama_Ekstrakulikuler" value="{{ $ekstrakulikuler->Nama_Ekstrakulikuler }}" disabled>
                     </div>
 
+                    <div class="form-group">
+                        <label for="Deskripsi">Deskripsi</label>
+                        <input type="text" class="form-control" id="Deskripsi" name="Deskripsi" value="{{ $ekstrakulikuler->Deskripsi }}" disabled>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-
+</div>
+</div>
+</div>
 </x-app-layout>
