1a2721a81b71796a5a26b4211fbeb91c17770a5f
Helfi23 <fie230401@gmail.com>
2023-05-31 16:57:08 +0700

    Update Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,8 +11,8 @@ class DashboardController extends Controller
     public function index(){
        $count_siswa = DB::table('siswa')->count();
        $count_guru = DB::table('guru')->count();
-       $count_kelas= DB::table('guru')->count();
-       $count_staff = DB::table('guru')->count();
+       $count_kelas= DB::table('kelas')->count();
+       $count_staff = DB::table('staff')->count();
         return view('dashboard',compact('count_siswa','count_guru','count_kelas','count_staff'));
     }
 }
