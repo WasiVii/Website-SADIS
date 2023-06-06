5aa99efeabc818b10b7762d1e7ec0aba92411c35
Abdul Wasi <abdul.wasi.al.afif@gmail.com>
2023-06-06 23:07:05 +0700

    Fix route command

--- app/Console/Commands/CreateRoutePermissionsCommand.php
+++ app/Console/Commands/CreateRoutePermissionsCommand.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Route;
+use Spatie\Permission\Models\Permission;
 
 class CreateRoutePermissionsCommand extends Command
 {
@@ -11,20 +13,44 @@ class CreateRoutePermissionsCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'app:create-route-permissions-command';
+    protected $signature = 'permission:create-permission-routes';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Create a permission routes.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
     /**
      * Execute the console command.
+     *
+     * @return int
      */
     public function handle()
     {
-        //
+        $routes = Route::getRoutes()->getRoutes();
+
+        foreach ($routes as $route) {
+            if ($route->getName() != '' && $route->getAction()['middleware']['0'] == 'web') {
+                $permission = Permission::where('name', $route->getName())->first();
+
+                if (is_null($permission)) {
+                    permission::create(['name' => $route->getName()]);
+                }
+            }
+        }
+
+        $this->info('Permission routes added successfully.');
     }
 }
